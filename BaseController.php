10edfb92b9319dc59c455c0ffeef55e0449af7b4
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-09-09 09:39:01 +0200

    Load additional language prefixes

--- BaseController.php
+++ BaseController.php
@@ -87,6 +87,14 @@ class BaseController extends \CI_Controller
      */
     public $langPrefix = "";
     /**
+     * Additional language prefix
+     *
+     * If you need more than one prefix, you can set it to this array.
+     *
+     * @var array
+     */
+    public $additionalPrefixes = array();
+    /**
      * Controller method
      *
      * @var string
@@ -164,6 +172,13 @@ class BaseController extends \CI_Controller
             }
 
             $this->_viewLoader->setLanguageStrings($this->langPrefix);
+
+            // check if we should include more than the on prefix
+            if (empty($this->additionalPrefixes) === false) {
+                foreach ($this->additionalPrefixes as $prefix) {
+                    $this->_viewLoader->setLanguageStrings($prefix);
+                }
+            }
         }
 
         // Load the sub-views
