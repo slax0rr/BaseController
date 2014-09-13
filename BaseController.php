323f89ac6af004265cfd4bd8360c0d7d5bc82320
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-09-13 22:06:34 +0200

    CRUD create method
    Takes data from POST, and tries to insert. This can not yet work,
    because base model is not prepared for such operations. Well, on success
    it would work, but when an error occurs it can not work.

--- BaseController.php
+++ BaseController.php
@@ -117,6 +117,25 @@ class BaseController extends \CI_Controller
      * @var \SlaxWeb\ViewLoader\Loader
      */
     protected $_viewLoader = null;
+
+    /*************************
+     * Basic CRUD properties *
+     *************************/
+    /**
+     * Create part of CRUD validation rules
+     *
+     * @var array
+     */
+    public $createRules = array();
+    /**
+     * View for after Create
+     *
+     * If left empty the default "create" method view will be used.
+     *
+     * @var string
+     */
+    public $afterCreate = "";
+
     /*************
      * Callbacks *
      *************/
@@ -168,6 +187,34 @@ class BaseController extends \CI_Controller
         } else {
             show_404();
         }
+    }
+
+    /****************************
+     * Basic CRUD functionality *
+     ****************************/
+    /**
+     * Create method
+     *
+     * Create part of crud, executed automatically.
+     * Method is auto accessed through POST request method.
+     */
+    public function create_post()
+    {
+        $data = $this->input->post();
+        $model = $this->router->{fetch_class()};
+        $this->{$model}->rules = $this->createRules;
+        $status = $this->{$model}->inset($data);
+
+        if ($status !== true) {
+            if ($error = $status->error("VALIDATION_ERROR")) {
+                return;
+            } elseif ($error = $status->error("CREATE_ERROR")) {
+                $this->viewData = array("createError" => $error->message);
+                return;
+            }
+        }
+
+        $this->view = $this->afterCreate;
     }
 
     /**
