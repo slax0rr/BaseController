8aedd6cc4f428400556053153c553a19b58f9347
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-11-10 16:42:53 +0100

    Add the config file

---
+++ install/slaxweb/basecontroller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * SlaxWeb BaseController config file
+ */
+
+/**
+ * Convert controller class case:
+ *
+ * 0 - leave as it is
+ * 1 - upper case first letter
+ * 2 - everything to lower case
+ */
+$config["controller_class_case"] = 0;
