6c951b45b0e62b1224623197972b3272a0e27de9
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-10-02 11:44:33 +0200

    add comments to existing test

--- Tests/BaseControllerTest.php
+++ Tests/BaseControllerTest.php
@@ -8,6 +8,12 @@ use \Mockery as m;
 
 class BaseControllerTest extends \PHPUnit_Framework_TestCase
 {
+    /*
+     * Test Missing Method
+     *
+     * When a method it missing, and custom 404 error method is not defined
+     * BaseController must call the CodeIgniter show_404 global function.
+     */
     public function testMissingMethod()
     {
         global $helperOutput;
@@ -21,20 +27,13 @@ class BaseControllerTest extends \PHPUnit_Framework_TestCase
         $c->_remap("missingMethod");
     }
 
-    public function testPostMethodRename()
-    {
-        global $helperOutput;
-        $helperOutput = true;
-
-        $c = $this->getMockBuilder("\\SlaxWeb\\BaseController\\BaseController")
-            ->setMethods(["_loadLanguage", "_loadViews", "_callback", "_loadModels"])
-            ->getMock();
-
-        $this->expectOutputRegex("~missingMethod_post~");
-        $c->input->server["REQUEST_METHOD"] = "POST";
-        $c->_remap("missingMethod");
-    }
-
+    /*
+     * Test Custom 404
+     *
+     * When a method is missing, and a custom 404 error method is defined,
+     * BaseController must call it, and load the views as if a normal
+     * controller method was called
+     */
     public function testCustom404()
     {
         global $helperOutput;
@@ -55,6 +54,33 @@ class BaseControllerTest extends \PHPUnit_Framework_TestCase
         $c->_remap("custom404");
     }
 
+    /*
+     * Test POST Method Rename
+     *
+     * When a request method POST is received, BaseController must append
+     * "_post" to the controller method.
+     */
+    public function testPostMethodRename()
+    {
+        global $helperOutput;
+        $helperOutput = true;
+
+        $c = $this->getMockBuilder("\\SlaxWeb\\BaseController\\BaseController")
+            ->setMethods(["_loadLanguage", "_loadViews", "_callback", "_loadModels"])
+            ->getMock();
+
+        $this->expectOutputRegex("~missingMethod_post~");
+        $c->input->server["REQUEST_METHOD"] = "POST";
+        $c->_remap("missingMethod");
+    }
+
+    /**
+     * Test Existing Method Remap
+     *
+     * When everything is alright with the request, and the method exists,
+     * it should be called, along with two callbacks and at the end the views
+     * must be loaded.
+     */
     public function testExistingMethodRemap()
     {
         $c = $this->getMockBuilder("\\SlaxWeb\\BaseController\\BaseController")
