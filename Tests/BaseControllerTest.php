71d8fbebd2ff23646df4cb1f3b9c6ee11c8e097a
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-10-02 10:00:16 +0200

    test 404 redirect on missing method

--- Tests/BaseControllerTest.php
+++ Tests/BaseControllerTest.php
@@ -1,11 +1,24 @@
 <?php
+namespace SlaxWeb\BaseController;
+
 require_once("Support/TestSupport.php");
 
 use \Mockery as m;
 
-class BaseControllerTest extends PHPUnit_Framework_TestCase
+function show_404()
 {
-    public function testRemap()
+    echo "missingMethod";
+}
+
+class BaseControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMissingMethod()
     {
+        $c = $this->getMockBuilder("\\SlaxWeb\\BaseController\\BaseController")
+            ->setMethods(["_loadLanguage", "_loadViews", "_callback", "_loadModels"])
+            ->getMock();
+
+        $this->expectOutputString("missingMethod");
+        $c->_remap("missingMethod");
     }
 }
