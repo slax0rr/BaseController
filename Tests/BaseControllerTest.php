abd0889bf209830cc3c5bce7e8b7fb49580e1c14
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-10-02 10:25:54 +0200

    test base controller renames method on post

--- Tests/BaseControllerTest.php
+++ Tests/BaseControllerTest.php
@@ -7,7 +7,14 @@ use \Mockery as m;
 
 function show_404()
 {
-    echo "missingMethod";
+    echo "show_404";
+}
+
+function method_exists($class, $method)
+{
+    echo "{$method}\n";
+
+    return \method_exists($class, $method);
 }
 
 class BaseControllerTest extends \PHPUnit_Framework_TestCase
@@ -15,10 +22,21 @@ class BaseControllerTest extends \PHPUnit_Framework_TestCase
     public function testMissingMethod()
     {
         $c = $this->getMockBuilder("\\SlaxWeb\\BaseController\\BaseController")
+            ->setMethods(["_loadLanguage", "_loadModels"])
+            ->getMock();
+
+        $this->expectOutputRegex("~show_404~");
+        $c->_remap("missingMethod");
+    }
+
+    public function testPostMethodRename()
+    {
+        $c = $this->getMockBuilder("\\SlaxWeb\\BaseController\\BaseController")
             ->setMethods(["_loadLanguage", "_loadViews", "_callback", "_loadModels"])
             ->getMock();
 
-        $this->expectOutputString("missingMethod");
+        $this->expectOutputRegex("~missingMethod_post~");
+        $c->input->server["REQUEST_METHOD"] = "POST";
         $c->_remap("missingMethod");
     }
 }
