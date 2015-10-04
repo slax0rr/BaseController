3fce19356195fc53e779b7dbc80e2af6edf94855
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-10-04 15:03:02 +0200

    add default values into config file

--- install/slaxweb/basecontroller.php
+++ install/slaxweb/basecontroller.php
@@ -11,3 +11,35 @@
  * 2 - everything to lower case
  */
 $config["controller_class_case"] = 0;
+
+/**
+ * Autoload views
+ *
+ * Available values: true/false
+ */
+$config["autoload_view"] = true;
+
+/**
+ * Template layout
+ *
+ * Available values:
+ * - false - do not use layouts
+ * - true - use a layout, and try to obtain the controller layout first,
+ * or use the application default layout
+ * - path/to/layout - path to a custom layout view
+ */
+$config["load_layout"] = false;
+
+/**
+ * Automatically load language file in every controller
+ *
+ * Available values: true/false
+ */
+$config["autoload_language"] = true;
+
+/**
+ * Autoload models
+ *
+ * Available values: true/false
+ */
+$config["autoload_model"] = true;
